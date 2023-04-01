b3b04d6f7a9f1a93061dc89d9eebda34080a5790
tbiuk <toni.biuk@gmail.com>
2023-04-01 11:27:56 +0200

    feat: email verification check

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Request, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { EmailVerifiedGuard } from './email-verified.guard';
 import { LocalAuthGuard } from './local-auth.guard';
 
 @Controller('auth')
@@ -7,6 +8,7 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @UseGuards(LocalAuthGuard)
+  @UseGuards(EmailVerifiedGuard)
   @Post('login')
   async login(@Request() req) {
     return this.authService.login(req.user);
---
+++ src/auth/email-verified.guard.ts
@@ -0,0 +1,27 @@
+import {
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { UsersRepository } from 'src/users/repositories/users.repository';
+import { JwtAuthGuard } from './jwt-auth.guard';
+
+@Injectable()
+export class EmailVerifiedGuard extends JwtAuthGuard {
+  constructor(private readonly usersRepository: UsersRepository) {
+    super();
+  }
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const user = await this.usersRepository.getUserByEmail(
+      request.body.username,
+    );
+
+    if (user && !user.verified) {
+      throw new UnauthorizedException('Email not verified');
+    }
+
+    return user && user.verified;
+  }
+}
